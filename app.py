8230048d4dd488f0bae5169fde3a8522644cdb75
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-01 00:07:13 +0100

    made into a flask file

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask,request, url_for, redirect, render_template
-import pickle
+
 import numpy as np
 
 app = Flask(__name__)
 
-model=pickle.load(open('model.pkl','rb'))
+
 
 @app.route('/')
-def home_page():
+def homePage():
     return render_template("index.html")
-
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    return
 
 if __name__ == '__main__':
     app.run(debug=True)
