092429f76fa9de1e1cc0ee7fa97ddd9e4672ca20
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-01 00:29:41 +0100

    minor updates

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def homePage():
+def dashboard():
     return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
