fc372216a51b75611b72424c14869eed330e3e6f
Borneel B. Phukan <borneelphukan@gmail.com>
2023-12-02 09:34:52 +0100

    dopamine optimizer integrated

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request, url_for, redirect, render_template
+from flask import Flask,request, url_for, redirect, render_template, jsonify
+from dopamine.optimizer import Dopamine
 
 import numpy as np
 
@@ -9,5 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def dashboard():
     return render_template("index.html")
+
+@app.route("/optimize", methods=["POST"])
+def optimize():
+    # Instantiate the Dopamine class and perform optimization
+    dopamine_optimizer = Dopamine()
+    # Perform optimization steps here using the Dopamine instance
+
+    # Dummy response, you can return any information you want
+    return jsonify({"message": "Optimization with Dopamine completed."})
+
 
 app.run(debug=True)
