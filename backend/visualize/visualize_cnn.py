8594c19ce8d677721c28ac831a02c075f68246bc
Borneel B. Phukan <borneelphukan@gmail.com>
2024-01-21 23:55:05 +0100

    c

--- backend/visualize/visualize_cnn.py
+++ backend/visualize/visualize_cnn.py
@@ -21,17 +21,17 @@ def training_loss(history):
     plt.savefig('static/images/visualization/training_loss.png')
 
 def test_accuracy(accuracy):
-    # Plot test accuracy
     plt.figure(figsize=(6, 6))
     plt.bar(['Test'], [accuracy], color=['blue'])
     plt.title('Test Accuracy')
     plt.ylabel('Accuracy')
+    plt.ylim([0, 1])  # Set y-axis limits to match the scale of accuracy (0 to 1)
     plt.savefig('static/images/visualization/test_accuracy.png')
 
 def test_loss(loss):
-    # Plot test loss
     plt.figure(figsize=(6, 6))
     plt.bar(['Test'], [loss], color=['red'])
     plt.title('Test Loss')
     plt.ylabel('Loss')
+    plt.ylim([0, 1])  # Set y-axis limits to match the scale of accuracy (0 to 1)
     plt.savefig('static/images/visualization/test_loss.png')
